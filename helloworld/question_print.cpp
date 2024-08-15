d858b2b0fea4600c5fc4028302f8b07b8728f4af
deadapi <lazy.curse@gmail.com>
2024-08-15 19:12:51 +0530

    hello-world

---
+++ helloworld/question_print.cpp
@@ -0,0 +1,21 @@
+/*
+Trying priting
+
+       1
+     2 3
+   4 5 6
+7 8 9 10
+*/
+
+
+//solution
+
+
+#include <iostream>
+
+int main(){
+    std::cout<<"      1\n";
+    std::cout<<"    2 3\n";
+    std::cout<<"  4 5 6\n";
+    std::cout<<"7 8 9 10\n";
+}
