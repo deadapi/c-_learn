891d1c10377240a1b07827d1b331e2da8216389c
deadapi <lazy.curse@gmail.com>
2024-08-17 02:24:00 +0530

    double trouble

--- #5data_types_basics/double/double.cpp
+++ #5data_types_basics/double/double.cpp
@@ -1,20 +1,4 @@
 /*
-    bool is a data type used to represent boolean values.
-    Values: It can only hold two values: true or false.
-*/
-
-
-#include <iostream>
-
-int main() {
-    bool isCodingFun = true;  // Declare a bool variable and initialize it to true
-    bool isCompleted = false;     // Declare another bool variable and initialize it to false
-
-    //as we go further we will read more about this
-    return 0;
-}
-
-/*
     The double data type is used to represent floating-point numbers, 
     which are numbers that have a fractional component (decimal point).
 */
