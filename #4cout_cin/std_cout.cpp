97975e3ff87600ec19c6ebc303055a3e272b26c8
deadapi <lazy.curse@gmail.com>
2024-08-16 23:53:10 +0530

    added few things

--- #4cout_cin/std_cout.cpp
+++ #4cout_cin/std_cout.cpp
@@ -11,4 +11,11 @@ int main(){
     std::cout<<"cartoon";//outputs cartoon
 
     //<<: Insertion operator that directs data to std::cout.
+
+    return 0; // Indicate successful execution
+    /*
+    In C++11 and later, if you omit the return 0; 
+    at the end of the main function, the compiler will automatically insert it for you.
+    so dont wry if i write return o; at end of code
+    */
 }
