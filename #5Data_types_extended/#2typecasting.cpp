f42d9866c1f64eb5d3d28372f1a4e698b0a5d3d0
deadapi <lazy.curse@gmail.com>
2024-08-17 01:54:40 +0530

    finished  basics

---
+++ #5Data_types_extended/#2typecasting.cpp
@@ -0,0 +1,18 @@
+//Type Casting: Changing a value from one type to another 
+//basics
+
+#include <iostream>
+
+int main(){
+
+    double  a =120212.2323;
+
+    int a1=(int)a;
+    //The notation (type) value means “convert value to type“.
+    //so first decide in which u need to type case then ...(type)variable
+
+    
+    //Note: Going from a double to an int simply removes the decimal. There’s no rounding involved.
+    
+
+}
