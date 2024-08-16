8886bdf114542bb9ddead5a816e870339d274b97
deadapi <lazy.curse@gmail.com>
2024-08-17 00:09:55 +0530

    added

---
+++ #5Data_types_extended/#1declaring_initializing.cpp
@@ -0,0 +1,16 @@
+//as we have previsly already saw int data type lets go a little deep
+
+#include <iostream>
+
+int main(){
+    int age = 28;
+
+    double price = 8.99;
+
+    char grade = 'A';
+
+    std::string message = "we code?";
+
+    bool is_it_true = true;
+
+}
